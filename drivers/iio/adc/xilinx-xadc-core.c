307e57902ad3b4d260379c5c6b29820462fdbec0
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-05-21 15:25:00 +0300

    iio: adc: xilinx-xadc-core: sync with ADI master
    Many of these changes were discarded in the ADI master tree during merge
    commit 02ff0f6543ec85044f3268ff237a0f75dc21aa64.
    
    This change also discards them in the rebased branch.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- drivers/iio/adc/xilinx-xadc-core.c
+++ drivers/iio/adc/xilinx-xadc-core.c
@@ -1255,7 +1255,7 @@ static int xadc_probe(struct platform_device *pdev)
 
 	ret = clk_prepare_enable(xadc->clk);
 	if (ret)
-		goto err_clk_disable_unprepare;
+		goto err_free_samplerate_trigger;
 
 	/*
 	 * Make sure not to exceed the maximum samplerate since otherwise the
@@ -1275,7 +1275,7 @@ static int xadc_probe(struct platform_device *pdev)
 	ret = request_irq(xadc->irq, xadc->ops->interrupt_handler, 0,
 			dev_name(&pdev->dev), indio_dev);
 	if (ret)
-		goto err_clk_disable_unprepare;
+		goto err_free_samplerate_trigger;
 
 	ret = xadc->ops->setup(pdev, indio_dev, xadc->irq);
 	if (ret)
