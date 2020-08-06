add335c52df2fbe430d4ea310bd849e47a0e2871
Michael Hennerich <michael.hennerich@analog.com>
2020-08-06 09:21:29 +0200

    iio: adc: ad9081: info print during  jesd_rx_calibrate_204c
    calibrate take 15-20 seconds per device - tell what's going on.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- drivers/iio/adc/ad9081.c
+++ drivers/iio/adc/ad9081.c
@@ -3069,6 +3069,7 @@ static int ad9081_jesd204_clks_enable(struct jesd204_dev *jdev,
 
 		if ((tx_lane_rate_kbps > 16230000UL) &&
 			phy->jesd_tx_link.lane_rate != tx_lane_rate_kbps) {
+			dev_info(dev, "running jesd_rx_calibrate_204c");
 			ret = adi_ad9081_jesd_rx_calibrate_204c(&phy->ad9081, 1, 0, 0);
 			if (ret < 0)
 				return ret;
